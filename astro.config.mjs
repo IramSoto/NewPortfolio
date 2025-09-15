153f586de6375770ef8817c6a06d6efbfd566f7f
IramSoto <85075405+IramSoto@users.noreply.github.com>
2025-09-15 17:27:36 -0600

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://iramsoto.github.io', // tu dominio de Pages de usuario
+  base: '/NewPortfolio/',             // nombre EXACTO del repo
+});
